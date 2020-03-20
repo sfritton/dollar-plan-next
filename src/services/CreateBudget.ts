ce01ae8a04890cf7b8864c81aef1f6281ef1da58
Sam Fritton <Samuel.Fritton@target.com>
2020-03-20 14:34:32 -0500

    updating services

--- src/services/CreateBudget.ts
+++ src/services/CreateBudget.ts
@@ -3,7 +3,10 @@ import { fetchPost } from '../util/fetch';
 import { PostgresId } from '../queries/types';
 
 async function CreateBudget(newBudget: Omit<Budget.Budget, 'id'>) {
-  return await fetchPost<Omit<Budget.Budget, 'id'>, PostgresId>('/api/budgets/create', newBudget);
+  return await fetchPost<Omit<Budget.Budget, 'id'>, PostgresId>(
+    'http://localhost:3000/budgets',
+    newBudget,
+  );
 }
 
 export default CreateBudget;
