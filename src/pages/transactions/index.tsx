53d987ec445434da8d62a23cd1322dcd23e8ab9b
Sam Fritton <Samuel.Fritton@target.com>
2020-11-10 15:26:40 -0600

    stylin' it up

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,33 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NextPage } from 'next';
 import Layout from '../../components/Layout';
 import Header from '../../containers/Header';
-import { ButtonPrimary } from '../../components/Button';
-import { InputText } from '../../components/Input';
 import { useAction } from '../../state/hooks';
 import searchTransactionsAction from '../../state/transactionSearchResults/searchTransactions';
 import { useSelector } from 'react-redux';
 import { getTransactionSearchResults } from '../../state/transactionSearchResults/selectors';
 import Transaction from '../../containers/Transaction';
+import InputSearch from '../../components/Input/InputSearch';
+import styles from './transactions.module.css';
 
 const TransactionsPage: NextPage = () => {
-  const [searchTerm, setSearchTerm] = useState('');
   const searchTransactions = useAction(searchTransactionsAction);
   const searchResults = useSelector(getTransactionSearchResults);
 
   return (
     <Layout.Grid>
       <Layout.Header>
-        <Header title="Search Transactions" />
+        <Header title="Transactions" />
       </Layout.Header>
       <Layout.Content>
-        <InputText value={searchTerm} onChange={setSearchTerm} label="Filter" />
-        <ButtonPrimary onClick={() => searchTransactions(searchTerm)}>Search</ButtonPrimary>
-        <ul>
-          {searchResults.map(id => (
-            <Transaction id={id} key={id} />
-          ))}
-        </ul>
+        <div className={styles.container}>
+          <InputSearch
+            className={styles.searchInput}
+            onSubmit={searchTransactions}
+            label="Search transactions"
+          />
+          {searchResults.length > 0 && (
+            <>
+              <h2 className={styles.resultsHeader}>
+                Found {searchResults.length} matching transactions
+              </h2>
+              <ul className={styles.results}>
+                {searchResults.map(id => (
+                  <Transaction id={id} key={id} />
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
       </Layout.Content>
     </Layout.Grid>
   );
